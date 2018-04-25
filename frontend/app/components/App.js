8bb3a8c2953c14b376e7185633efab581a8bf0e3
Arkady Arkhangorodsky <aarkhangorodsky@gmail.com>
2018-04-25 21:32:52 +0300

    Implemented basic routing

--- frontend/app/components/App.js
+++ frontend/app/components/App.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom';
 
 require('./App.css');
+
+const Lasagna = ({ match }) => (
+    <div>
+      <p>Hi my name is Lasagna number {match.params.lasagnaId}</p>
+    </div>
+);
+
+const Home = () => (
+  <div>
+    <h1>I think dogs should vote!</h1>
+    <ul>
+      <li><Link to='/lasagna/1'>1</Link></li>
+      <li><Link to='/lasagna/2'>2</Link></li>
+      <li><Link to='/lasagna/3'>3</Link></li>
+    </ul>
+  </div>
+);
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,7 +32,12 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <h1>I think dogs should vote!</h1>
+      <Router>
+        <div>
+          <Route exact path='/' component={Home}/>
+          <Route path='/lasagna/:lasagnaId' component={Lasagna}/>
+        </div>
+      </Router>
     );
   }
 }
